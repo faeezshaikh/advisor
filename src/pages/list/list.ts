8d7d96f519d550a7dd49e0b322661ccd90029986
Faeez Shaikh <faeez.shaikh@gmail.com>
2019-03-26 17:43:22 -0500

    updates

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { Observable } from 'rxjs';
 import { HelperProvider } from '../../providers/helper/helper';
 
 @Component({
@@ -9,30 +9,28 @@ import { HelperProvider } from '../../providers/helper/helper';
 })
 export class ListPage {
   selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  // icons: string[];
+  items: Observable<any[]>;
+  // items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,public helper:HelperProvider) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
 
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+    // this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    // 'american-football', 'boat', 'bluetooth', 'build'];
 
-    this.helper.getItems().subscribe(res => {
-      console.log('Results:',res);
-      
-    });
+    // this.items = [];
+    // for (let i = 1; i < 11; i++) {
+    //   this.items.push({
+    //     title: 'Item ' + i,
+    //     note: 'This is item #' + i,
+    //     icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+    //   });
+    // }
+
+    this.items = this.helper.getItems();
   }
 
   itemTapped(event, item) {
